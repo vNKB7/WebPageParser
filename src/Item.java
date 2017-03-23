f824b32ba27f0f6a64c6afbcb861cb9b1aafc2f2
Wendiven <szhjzwz@gmail.com>
2017-03-23 12:33:26 +0800

    to merge

--- src/Item.java
+++ src/Item.java
@@ -1,5 +1,12 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Node;
 
 public class Item {
 	public String value;
@@ -13,28 +20,77 @@ public class Item {
 		value = v;
 		prefix = p;
 	}
-	
-	public static void main(String[] args) {
-		ArrayList<Item> array = new ArrayList<Item>();
-		
-		String value = "重庆开疆建筑工程有限公司";
-		ArrayList<String> prefix = new ArrayList<String>();
-		prefix.add("綦江区2016年横山高标准农田建设项目");
-		prefix.add("一标段");
-		prefix.add("第一中标候选人");
-		
-		
-		Item item = new Item(value, prefix);
-		
-		array.add(item);
-		
-		for(Item it : array){
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		File input = new File(".//t.html");
+		Document doc = Jsoup.parse(input, "Utf-8");
+		Element body = doc.body();
+//		Element e = body.select("table").first();
+		ArrayList<Item> l = new ArrayList<Item>();
+		ArrayList<Element> stack = new ArrayList<Element>();
+		ArrayList<Integer> container1 = new ArrayList<Integer>();
+		ArrayList<String> container2 = new ArrayList<String>();
+		for (int k = 0; k < 20; k ++){
+			container1.add(0);
+			container2.add("");
+		}
+		Element root = body;
+		stack.add(root);
+		boolean a = true;
+		while(stack.size() > 0){
+			Element node = stack.get(stack.size() - 1);
+			stack.remove(stack.size() - 1);
+			if (node.children().size() != 0 && !node.child(0).attr("width").equals("") && !node.child(0).attr("width").equals("100%")){
+				a = false;
+			}
+			if (a){
+				for (int i = node.children().size() - 1; i >= 0; i--){
+					stack.add(node.child(i));
+				}
+			 } else {
+				 String value = node.child(node.children().size() - 1).text();
+				 ArrayList<String> prefix = new ArrayList<String>();
+				 int N = node.children().size() - 1;
+				 int count = 0;
+				 for (int i = 0; i < N; i++){
+					 while(container1.get(i) != 0){
+						 prefix.add(container2.get(i));
+						 container1.set(i, container1.get(i) - 1);
+						 if (container1.get(i) == 0){
+							 container2.set(i,"");
+						 }
+						 i += 1;
+						 N += 1;
+						 count += 1;
+					 }
+					 prefix.add(node.child(i - count).text());
+					 if (i + 1 == N && container1.get(i + 1) != 0){
+						 prefix.add(container2.get(i + 1));
+						 container1.set(i, container1.get(i + 1) - 1);
+						 if (container1.get(i + 1) == 0){
+							 container2.set(i + 1,"");
+						 }
+					 }
+					 if (!node.child(i - count).attr("rowspan").equals("")){
+						 container1.set(i, Integer.valueOf(node.child(i - count).attr("rowspan")) - 1);
+						 container2.set(i, node.child(i - count).text());
+					 }
+				 }
+				 Item item = new Item(value, prefix);
+				 l.add(item);
+				 a = true;
+			 }
+		}
+		for(Item it : l){
+			if (it.value.equals("")) continue;
 			System.out.print(it.value+"	");
 			for(int i = 0; i < it.prefix.size(); i++){
 				System.out.print(it.prefix.get(i)+";");
 			}
+			System.out.println();
 		}
-
+		
 	}
 
 }
